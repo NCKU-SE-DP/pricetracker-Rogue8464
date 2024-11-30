b36b6a5488af91eae2c6b79f643cbb866f7c6c57
Rogue8464 <rogue.8464@gmail.com>
2024-11-30 21:39:15 +0800

    refactor: extract OpenAI functions into OPENAIClient

---
+++ backend/src/llm_client/base.py
@@ -0,0 +1,28 @@
+import abc
+from src.crawler.exceptions import DomainMismatchException
+from pydantic import BaseModel, Field
+
+class MessagePassingInterfaceExample(BaseModel):
+    role: str = Field(
+        default=...,
+        example="example",
+        description="description"
+    )
+    content: str
+
+class LLMClientBase(metaclass=abc.ABCMeta):
+    
+    @abc.abstractmethod
+    def sum_up_news() -> dict:
+        
+        return NotImplemented
+    
+    @abc.abstractmethod
+    def sum_up_news() -> str:
+        
+        return NotImplemented
+    
+    @abc.abstractmethod
+    def evaluate_relevance() -> str:
+        
+        return NotImplemented
